c4fd7986a068323304948047425eb8e95c9f9036
claudiosgomez <claudio.gomez.1991.s@gmail.com>
2021-01-12 22:53:45 +0100

    Enemy Follows Player

--- PreAlpha/Assets/Scripts/Enemy.cs
+++ PreAlpha/Assets/Scripts/Enemy.cs
@@ -1,12 +1,30 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //Enemy Variables
+    Rigidbody2D rigidbody2D;
+
+    //Player variables
+    [SerializeField] Transform player;
+    
+    //Attack variables
     [SerializeField] float health = 20;
-    [SerializeField] float pushForce = 1;
     [SerializeField] float damage = 1;
+    [SerializeField] float pushForce = 1;
+
+    //Receive damage variables
+    [SerializeField] float agroRange;
+    [SerializeField] float moveSpeed;
+
+    private void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
     public float GetForce()
     {
         return pushForce;
@@ -25,16 +43,45 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
+        //Distance to player
+        float distanceToPlayer = Vector2.Distance(transform.position, player.position);
+
+        //Check if it is within the agro area
+        if(distanceToPlayer < agroRange)
+        {
+            //Chase player
+            ChasePlayer();
+        }
+        else
+        {
+            //Stop chasing player
+            StopChasingPlayer();
+        }
+
+
+        //Is dead?
         if (health <= 0)
         {
             Destroy(gameObject);
         }
     }
 
-
-
-    private void OnTriggerEnter2D(Collider2D collision)
+    private void StopChasingPlayer()
     {
-        Debug.Log("I See you " + collision.name.ToString());
+       
+    }
+
+    private void ChasePlayer()
+    {
+        if(transform.position.x < player.position.x)
+        {
+            //enemy is to the left side to the player, so move right
+            rigidbody2D.velocity = new Vector2(moveSpeed, 0);
+        }
+        else
+        {
+            //enemy is to the right side to the player, so move left
+            rigidbody2D.velocity = new Vector2(-moveSpeed, 0);
+        }
     }
 }
