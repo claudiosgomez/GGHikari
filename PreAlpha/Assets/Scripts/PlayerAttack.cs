a65e5d7b2b3fc7e89a281c9e06d168f50492c528
claudiosgomez <claudio.gomez.1991.s@gmail.com>
2021-05-05 20:04:40 +0200

    Slash Animation
    Included slash animation and increased the range of the attack so it is logical to the animation size

--- PreAlpha/Assets/Scripts/PlayerAttack.cs
+++ PreAlpha/Assets/Scripts/PlayerAttack.cs
@@ -8,29 +8,33 @@ public class PlayerAttack : MonoBehaviour
     private float timeBetweenAttack;
     [SerializeField] float StartTimeBetweenAttack = 1f;
     Animator animator;
-
+    SpriteRenderer playerSprite;
     [SerializeField] public Transform attackPosition;
     [SerializeField] public LayerMask whatIsEnemy;
     [SerializeField] float attackRange = 5f;
     [SerializeField] float damage = 10f;
+    [SerializeField] GameObject SlashAttack;
+    [SerializeField] GameObject mySlashAttack;
 
     private void Start()
     {
         animator = gameObject.GetComponent<Animator>();
+        playerSprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-            if (Input.GetKeyDown("left ctrl"))
-            {
-            
+
+        if (Input.GetKeyDown("left ctrl"))
+        {
+
             if (timeBetweenAttack <= 0)
             {
-                
+
                 animator.Play("Attack");
                 Collider2D[] enemiesToDamage = Physics2D.OverlapCircleAll(attackPosition.position, attackRange, whatIsEnemy);
+             
 
                 for (int i = 0; i < enemiesToDamage.Length; i++)
                 {
@@ -39,7 +43,7 @@ public class PlayerAttack : MonoBehaviour
                 timeBetweenAttack = StartTimeBetweenAttack;
             }
 
-            
+
         }
         else
         {
@@ -50,6 +54,21 @@ public class PlayerAttack : MonoBehaviour
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPosition.position,attackRange);
+        Gizmos.DrawWireSphere(attackPosition.position, attackRange);
+    }
+
+    public void SlashAnimation()
+    {
+        Debug.Log(playerSprite.flipX.ToString());
+        if (playerSprite.flipX)
+        {
+            mySlashAttack = (GameObject)Instantiate(SlashAttack, new Vector3(attackPosition.transform.position.x - 1.206f, attackPosition.transform.position.y, attackPosition.transform.position.z), Quaternion.Euler(new Vector3(0, 180, 0)));
+        }
+        else
+        {
+            mySlashAttack = (GameObject)Instantiate(SlashAttack, new Vector3(attackPosition.transform.position.x, attackPosition.transform.position.y, attackPosition.transform.position.z), Quaternion.Euler(new Vector3(0, 0, 0)));
+        }
+        
+        Destroy(mySlashAttack, 0.2f);
     }
 }
